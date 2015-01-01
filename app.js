c7ee5038903467422550c25a3dd825266b7b73a2
Josh Cai <yuanshi419@gmail.com>
2015-01-01 01:27:57 -0600

    add react and jquery library with bower

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower', express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
 app.use('/api', api_routes);
